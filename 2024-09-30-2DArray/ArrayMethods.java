5bd23dca58f873e2ec7a12dab49fedb6f1b80d20
Leonardo Benitez <lbenitez60@stuy.edu>
2024-10-01 10:51:54 -0400

    added comments

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -20,6 +20,7 @@ public static String arrToString(int[] nums){
     returnedString = returnedString + "]";
     return returnedString;
 }
+
 //3. Write arrToString with a 2D array parameter.
 /**Return a String that represents the 2D array in the format:
   * "[[2, 3, 4], [5, 6, 7], [2, 4, 9]]"
@@ -27,24 +28,28 @@ public static String arrToString(int[] nums){
 public static String arrToString(int[][] ary){
     String result = "[";
     for(int i = 0; i < ary.length; i++){
+        // Use the arrToString for 1D arrays to handle each row
         result += arrToString(ary[i]);
         if (i < ary.length - 1) {
-            result += ", ";
+            result += ", "; // Add comma and space between arrays
         }
     }
     result += "]";
     return result;
 }
+
 //4. Return the sum of all of the values in the 2D array
 public static int arr2DSum(int[][] nums){
     int sum = 0;
+    // Use nested loops to go through each element in the 2D array
     for (int i = 0; i < nums.length; i++){
         for (int j = 0; j < nums[i].length; j++){
-            sum += nums[i][j];
+            sum += nums[i][j]; // Add each element to the sum
         }
     }
     return sum;
 }
+
 /**Rotate an array by returning a new array with the rows and columns swapped.
   * You may assume the array is rectangular and neither rows nor cols is 0.
   * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
@@ -52,10 +57,11 @@ public static int arr2DSum(int[][] nums){
 public static int[][] swapRC(int[][] nums){
     int rowCount = nums.length;
     int colCount = nums[0].length;
-    int[][] swapped = new int[colCount][rowCount];
+    int[][] swapped = new int[colCount][rowCount]; // Create a new array with swapped dimensions
+
     for (int i = 0; i < rowCount; i++){
         for (int j = 0; j < colCount; j++){
-            swapped[j][i] = nums[i][j];
+            swapped[j][i] = nums[i][j]; // Swap rows and columns
         }
     }
     return swapped;
