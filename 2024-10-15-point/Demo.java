ea22d4fed16c8e44d4f8a5cfb9e2218c73f3bf1a
Leonardo Benitez <lbenitez60@stuy.edu>
2024-10-15 08:47:31 -0500

    fixed distance method

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -5,7 +5,7 @@ public class Demo{
   to the others.
   */
   public static double distance(Point a, Point b){
-    return Math.sqrt(a * a + b * b);
+    return Math.sqrt(Math.pow(b.getY() - a.getY(),2) + Math.pow(b.getX() - a.getX(),2));
   }
 
   public static void main(String[]args){
