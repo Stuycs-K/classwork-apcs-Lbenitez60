65b2cb495f29157fc4414a63e05287aa49d53bcc
Leonardo Benitez <lbenitez60@stuy.edu>
2024-10-15 08:44:44 -0500

    Added distance method

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -5,7 +5,7 @@ public class Demo{
   to the others.
   */
   public static double distance(Point a, Point b){
-    return 0.0;
+    return Math.sqrt(a * a + b * b);
   }
 
   public static void main(String[]args){
