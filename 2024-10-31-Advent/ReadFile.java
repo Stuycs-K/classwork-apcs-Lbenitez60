9d568fab11401d59e55927975976df4971f94a27
Leonardo Benitez <lbenitez60@stuy.edu>
2024-10-31 08:56:40 -0500

    try/catch + file reading.

--- 2024-10-31-Advent/ReadFile.java
+++ 2024-10-31-Advent/ReadFile.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class ReadFile{
   public static void main(String[] args){
@@ -9,5 +11,18 @@ public class ReadFile{
     while(sc2.hasNextDouble())
       sum+=sc2.nextDouble();
     System.out.println(sum);
+    try {
+      File file = new File("ReadFile.java");//1
+      Scanner input = new Scanner(file);
+      //CODE THAT SCANS THE FILE.
+      while (input.hasNextLine())
+        System.out.println(input.nextLine());
+      input.close();//releases the file from your program
+
+    } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
+      return; //you can return from a void function just don't put a value.
+    }
   }
 }
