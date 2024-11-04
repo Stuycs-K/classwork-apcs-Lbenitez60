e32539282407a27e81a809762932df007f326f3f
Leonardo Benitez <lbenitez60@stuy.edu>
2024-11-04 07:13:46 -0500

    try catch block.

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -1,17 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class TriangleTester{
   public static void main(String[] args){
-    countTrianglesA("inputTri.txt")
+    countTrianglesA("inputTri.txt");
   }
   public static int countTrianglesA(String filename){
-    File file = new File(filename);
-    Scanner input = new Scanner(file);
-    int count = 0;
-    while (input.hasNextLine){
-      int a = input.nextInt();
-      int b = input.nextInt();
-      int c = input.nextInt();
-      if(triangleTest(a,b,c))
-        count++;
+    try{
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      int count = 0;
+      while (input.hasNextLine()){
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        if(triangleTest(a,b,c))
+          count++;
+      }
+      return count;
+    } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
+      return 0;
     }
   }
   public static boolean triangleTest(int a, int b, int c){
