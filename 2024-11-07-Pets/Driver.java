d7edfe9cde23ec48c8d0e940bddb3f9b0dcdb9b5
Leonardo Benitez <lbenitez60@stuy.edu>
2024-11-07 08:56:44 -0600

    tested bird

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -2,5 +2,7 @@ public class Driver{
   public static void main(String[] args){
     Animal abc = new Animal ("oop",66,"bob");
     abc.speak();
+    Bird bill = new Bird("squawk", 40, "bill", 3.3, "red");
+    bill.speak();
   }
 }
