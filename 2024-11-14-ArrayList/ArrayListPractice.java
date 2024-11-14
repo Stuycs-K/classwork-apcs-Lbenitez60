a6bcb97681ef9f2e7fccf7c0ece40a3a9f204613
Leonardo Benitez <lbenitez60@stuy.edu>
2024-11-14 10:14:10 -0500

    more work.

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-
 public class ArrayListPractice{
-
   public static ArrayList<String> createRandomArray(int size){
     ArrayList<String> result = new ArrayList<String>(size);
     for (int i = 0; i < size; i++){
@@ -13,14 +11,25 @@ public class ArrayListPractice{
   }
   public static void replaceEmpty( ArrayList<String> original){
     for (int i=0; i < original.size();i++) {
-    if (original.get(i).equals("")) {
-      original.set(i,"Empty");
+      if (original.get(i).equals("")) {
+        original.set(i,"Empty");
+      }
     }
   }
-}
   public static ArrayList<String> makeReversedList( ArrayList<String> original){
-  ArrayList<String> result = new ArrayList<String>(original.size());
-  //return a new ArrayList that is in the reversed order of the original.
-  for(int i=original.size(); i< original.size;)
-}
+    ArrayList<String> result = new ArrayList<String>(original.size());
+    //return a new ArrayList that is in the reversed order of the original.
+    for(int i=original.size()-1; i>= 0;i--){
+      result.add(original.get(i));
+    }
+    return result;
+  }
+  public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+    ArrayList<String> result = new ArrayList<String>(a.size()+b.size());
+    for (int i = 0; i < a.size() && i < b.size; i++){
+      result.add(a.get(i)).add(b.get(i));
+    }
+    //add extra values;
+    return result;
+  }
 }
--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 public class Driver {
   public static void main(String[] args) {
     System.out.println(ArrayListPractice.createRandomArray(10));
-    ArrayList<String> a1 = ArrayListPractice.createRandomArray(100);
+    ArrayList<String> a1 = ArrayListPractice.createRandomArray(10);
     System.out.println(a1);
     ArrayListPractice.replaceEmpty(a1);
     System.out.println(a1);
-
+    System.out.println(ArrayListPractice.makeReversedList(a1));
   }
 }
