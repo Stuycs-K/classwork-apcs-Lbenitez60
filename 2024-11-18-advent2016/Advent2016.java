5a2979f1bde36284278e60e3710e38e71d769902
Leonardo Benitez <lbenitez60@stuy.edu>
2024-11-18 10:10:56 -0500

    fixed compile issues

--- 2024-11-18-advent2016/Advent2016.java
+++ 2024-11-18-advent2016/Advent2016.java
@@ -1,5 +1,25 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
 public class Advent2016{
   public static void main (String[] args){
-
+    read("puzzleInput.txt");
+  }
+  public static ArrayList<String> read(String filename){
+    try {
+      File file = new File(filename);//1
+      Scanner input = new Scanner(file);
+      ArrayList<String> result = new ArrayList<String>(0);
+      while (input.hasNext()){
+        result.add(input.next());
+      }
+      input.close();
+      return result;
+    } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
+      return new ArrayList<String>();
+    }
   }
 }
