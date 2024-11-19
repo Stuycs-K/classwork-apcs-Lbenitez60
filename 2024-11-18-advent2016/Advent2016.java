0e469284638a7d3e1c4351ff1f5703943878af16
Leonardo Benitez <lbenitez60@stuy.edu>
2024-11-19 15:35:03 -0500

    finished day1 part two started day 2

--- 2024-11-18-advent2016/Advent2016.java
+++ 2024-11-18-advent2016/Advent2016.java
@@ -2,9 +2,11 @@ import java.io.File;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 public class Advent2016{
   public static void main (String[] args){
-    System.out.println(day1());
+    System.out.println("Day1.1: "+day1(0));
+    System.out.println("Day1.2: "+day1(1));
   }
   public static ArrayList<String> read(String filename){
     try {
@@ -22,26 +24,45 @@ public class Advent2016{
       return new ArrayList<String>();
     }
   }
-  public static int day1(){
+
+  public static int day2(int p){
+    int[][] keypad = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
     ArrayList<String> steps = read("puzzleInput.txt");
+    return 0;
+  }
+
+
+
+
+  public static int day1(int p){
+    ArrayList<String> steps = read("d1input.txt");
     for (int i = 0; i < steps.size()-1; i ++){
       steps.set(i,steps.get(i).substring(0,steps.get(i).length()-1));
     }
     int x=0,y=0,dir=0;
     int[][]offset={{0,1},{1,0},{0,-1},{-1,0}};
+    // int[][] locations = new int[10000][2];
+    ArrayList<int[]> locations= new ArrayList<int[]>();
+    int count = 0;
     for (int i=0; i < steps.size();i++){
       if (steps.get(i).substring(0,1).equals("L"))
         dir=(dir-1)%4;
       if (steps.get(i).substring(0,1).equals("R"))
         dir=(dir+1)%4;
       if (dir<0) dir += 4;
-      System.out.println("dir:"+dir);
       int dis = Integer.parseInt(steps.get(i).substring(1));
-      System.out.println("dis"+dis);
-      x += offset[dir][0] * dis;
-      y += offset[dir][1] * dis;
-      System.out.println("x:"+x);
-      System.out.println("y:"+y);
+      //x += offset[dir][0] * dis;
+      for (int j = 0; j < dis; j ++){
+        x+=offset[dir][0];
+        y+=offset[dir][1];
+        for (int k = 0; k < count; k++){
+          if (x==locations.get(k)[0]&&y==locations.get(k)[1]&&p==1){
+            return Math.abs(x)+Math.abs(y);
+          }
+        }
+        locations.add(new int[] {x,y});
+        count++;
+      }
     }
     return Math.abs(x)+Math.abs(y);
   }
