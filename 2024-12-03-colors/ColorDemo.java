041e7fffe30109b227ab3745675cc136b26f29c9
Leonardo Benitez <lbenitez60@stuy.edu>
2024-12-03 23:44:42 -0500

    more constants, started writing for loops

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -4,3 +4,30 @@ public class ColorDemo {
     public static final String HIDE_CURSOR = "\u001b[?25l";
     public static final String SHOW_CURSOR = "\u001b[?25h";
     public static final String RESET = "\u001b[0m";
+
+    // Colors
+    public static final int BLACK = 30;
+    public static final int RED = 31;
+    public static final int GREEN = 32;
+    public static final int YELLOW = 33;
+    public static final int BLUE = 34;
+    public static final int MAGENTA = 35;
+    public static final int CYAN = 36;
+    public static final int WHITE = 37;
+
+    public static void main(String[] args) {
+        System.out.print(CLEAR_SCREEN + HIDE_CURSOR);
+
+        // Colorful demo
+        for (int i = 0; i < 10; i++) {
+            go(5, 10); // Move to position (row 5, column 10)
+            color(RED, BLACK);
+            System.out.print("Red Text");
+            
+            go(6, 10);
+            color(GREEN, BLACK);
+            System.out.print("Green Text");
+
+            go(7, 10);
+            color(BLUE, BLACK);
+            System.out.print("Blue Text");
