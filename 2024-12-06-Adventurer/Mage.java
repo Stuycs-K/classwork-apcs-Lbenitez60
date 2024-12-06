93be611d74275e6bbe5f00c2e55b674d556c92de
Leonardo Benitez <lbenitez60@stuy.edu>
2024-12-06 09:59:01 -0500

    fixed initial compile issues

--- 2024-12-06-Adventurer/Mage.java
+++ 2024-12-06-Adventurer/Mage.java
@@ -5,25 +5,41 @@ public class Mage extends Adventurer{
   public Mage(String name, int hp){
       super(name,hp);
   }
-  public String getSpecialName();
+  public String getSpecialName(){
+    return "";
+  }
   //accessor methods
-  public int getSpecial();
-  public void setSpecial(int n);
-  public int getSpecialMax();
+  public int getSpecial(){
+    return 0;
+  }
+  public void setSpecial(int n){
+
+  }
+  public int getSpecialMax(){
+    return 0;
+  }
 
   /*
     all adventurers must have a way to attack enemies and
     support their allys
   */
   //hurt or hinder the target adventurer
-  public String attack(Adventurer other);
+  public String attack(Adventurer other){
+    return "";
+  }
 
   //heall or buff the target adventurer
-  public String support(Adventurer other);
+  public String support(Adventurer other){
+    return "";
+  }
 
   //heall or buff self
-  public String support();
+  public String support(){
+    return "";
+  }
 
   //hurt or hinder the target adventurer, consume some special resource
-  public String specialAttack(Adventurer other);
+  public String specialAttack(Adventurer other){
+    return "";
+  }
 }
