a8470c4be90ee416cd1834b2b51716607aebcaab
Leonardo Benitez <lbenitez60@stuy.edu>
2025-01-03 10:10:44 -0500

    started makwords

--- 2025-01-02/Recursion.java
+++ 2025-01-02/Recursion.java
@@ -1,11 +1,19 @@
+import java.util.Arrays;
 public class Recursion{
   public static void main(String[] args) {
-    System.out.println(fib(Integer.parseInt(args[0])));
+    // System.out.println(fib(Integer.parseInt(args[0])));
+    makeWords(0,"","abc");
   }
   public static int fib(int n){
     if(n<2)
       return n;
     else
       return fib(n-1)+fib(n-2);
+  }
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+    if(remainingLetters==0)
+      System.out.println(result);
+    String[] alpList = alphabet.split("");
+    System.out.println(Arrays.toString(alpList));
   }
 }
